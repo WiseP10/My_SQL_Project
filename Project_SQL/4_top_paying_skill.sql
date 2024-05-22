30f0996a0366c037b52f7fc49d0a106ac0bf7a22
WiseP10 <pha_tit@hotmail.com>
2024-05-22 12:01:30 +0700

    modified:   Project_SQL/4_top_paying_skill.sql 	modified:   Project_SQL/5_optimal_skills.sql 	modified:   README.md 	new file:   asset/skill_for_top_pay.png 	new file:   asset/skill_in_demand.png 	new file:   asset/skill_value.png 	new file:   asset/top_optimal_skill.png

--- Project_SQL/4_top_paying_skill.sql
+++ Project_SQL/4_top_paying_skill.sql
@@ -6,11 +6,11 @@ FROM
     JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
 WHERE 
-    job_title_short = 'Business Analyst' AND
+    job_title_short = 'Data Analyst' AND
     salary_year_avg IS NOT NULL
     AND job_work_from_home = TRUE
 GROUP BY skills
 order by 
     avg_salary DESC
 
-limit 25
+limit 10
--- Project_SQL/5_optimal_skills.sql
+++ Project_SQL/5_optimal_skills.sql
@@ -19,4 +19,4 @@ HAVING
 ORDER BY
     avg_salary DESC,
     demand_count DESC
-LIMIT 25;
+LIMIT 10;
